fddf9bdf152df5352d5501d4216600788602330a
ieva404 <javainis@gmail.com>
2023-03-05 19:09:49 +0200

    third commit

--- app/app.js
+++ app/app.js
@@ -5,4 +5,15 @@ app.use(cors());
 const app = express();
 app.use(express.json());
 
-app.listen(3000);
+app.get("/skills", (req, res) => {
+  res.json([
+    { skill: "hard-working" },
+    { skill: "team-player" },
+    { skill: "quick-learner" },
+    { skill: "result-oriented" },
+  ]);
+});
+
+app.listen(3000, () => {
+  console.log("my -add skill- server started");
+});
